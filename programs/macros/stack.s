97e5caadbb3d01e9b35e469818101526042413be
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2022-10-19 13:27:13 -0700

    updated with inline comments, and fixed some cut-n-paste oversights

--- programs/macros/stack.s
+++ programs/macros/stack.s
@@ -44,99 +44,108 @@
 # Push Macros
 
 .macro push(%r0)
-   addiu $sp, $sp, -4
-   sw %r0, 0($sp)
+        nop                     # Pushing one register
+        addiu $sp, $sp, -4      # 1 * 4 bytes = 4
+        sw %r0, 0($sp)
 .end_macro
 
 .macro push(%r0, %r1)
-   addiu $sp, $sp, -8
-   sw %r1, 4($sp)
-   sw %r0, 0($sp)
+        nop                     # Pushing two registers
+        addiu $sp, $sp, -8      # 2 * 4 bytes = 8
+        sw %r1, 4($sp)
+        sw %r0, 0($sp)
 .end_macro
 
 .macro push(%r0, %r1, %r2)
-   addiu $sp, $sp, -12
-   sw %r3, 12($sp)
-   sw %r2, 8($sp)
-   sw %r1, 4($sp)
-   sw %r0, 0($sp)
+        nop                     # Pushing three registers
+        addiu $sp, $sp, -12     # 3 * 4 bytes = 12
+        sw %r2, 8($sp)
+        sw %r1, 4($sp)
+        sw %r0, 0($sp)
 .end_macro
 
 .macro push(%r0, %r1, %r2, %r3)
-   addiu $sp, $sp, -16
-   sw %r3, 12($sp)
-   sw %r2, 8($sp)
-   sw %r1, 4($sp)
-   sw %r0, 0($sp)
+        nop                     # Pushing four registers
+        addiu $sp, $sp, -16     # 4 * 4 bytes = 16
+        sw %r3, 12($sp)
+        sw %r2, 8($sp)
+        sw %r1, 4($sp)
+        sw %r0, 0($sp)
 .end_macro
 
 .macro push(%r0, %r1, %r2, %r3, %r4)
-   addiu $sp, $sp, -20
-   sw %r4, 16($sp)
-   sw %r3, 12($sp)
-   sw %r2, 8($sp)
-   sw %r1, 4($sp)
-   sw %r0, 0($sp)
+        nop                     # Pushing five registers
+        addiu $sp, $sp, -20     # 5 * 4 bytes = 20
+        sw %r4, 16($sp)
+        sw %r3, 12($sp)
+        sw %r2, 8($sp)
+        sw %r1, 4($sp)
+        sw %r0, 0($sp)
 .end_macro
 
 .macro push(%r0, %r1, %r2, %r3, %r4, %r5)
-   addiu $sp, $sp, -24
-   sw %r5, 20($sp)
-   sw %r4, 16($sp)
-   sw %r3, 12($sp)
-   sw %r2, 8($sp)
-   sw %r1, 4($sp)
-   sw %r0, 0($sp)
+        nop                     # Pushing 6 registers
+        addiu $sp, $sp, -24     # 6 * 4 bytes = 24
+        sw %r5, 20($sp)
+        sw %r4, 16($sp)
+        sw %r3, 12($sp)
+        sw %r2, 8($sp)
+        sw %r1, 4($sp)
+        sw %r0, 0($sp)
 .end_macro
 
 .macro push(%r0, %r1, %r2, %r3, %r4, %r5, %r6)
-   addiu $sp, $sp, -28
-   sw %r6, 24($sp)
-   sw %r5, 20($sp)
-   sw %r4, 16($sp)
-   sw %r3, 12($sp)
-   sw %r2, 8($sp)
-   sw %r1, 4($sp)
-   sw %r0, 0($sp)
+        nop                     # Pushing 7 registers
+        addiu $sp, $sp, -28     # 7 * 4 bytes = 28
+        sw %r6, 24($sp)              
+        sw %r5, 20($sp)
+        sw %r4, 16($sp)
+        sw %r3, 12($sp)
+        sw %r2, 8($sp)
+        sw %r1, 4($sp)
+        sw %r0, 0($sp)
 .end_macro
 
 .macro push(%r0, %r1, %r2, %r3, %r4, %r5, %r6, %r7)
-   addiu $sp, $sp, -32
-   sw %r7, 28($sp)
-   sw %r6, 24($sp)
-   sw %r5, 20($sp)
-   sw %r4, 16($sp)
-   sw %r3, 12($sp)
-   sw %r2, 8($sp)
-   sw %r1, 4($sp)
-   sw %r0, 0($sp)
+        nop                     # Pushing 8 registers
+        addiu $sp, $sp, -32     # 8 * 4 bytes = 32
+        sw %r7, 28($sp)
+        sw %r6, 24($sp)
+        sw %r5, 20($sp)
+        sw %r4, 16($sp)
+        sw %r3, 12($sp)
+        sw %r2, 8($sp)
+        sw %r1, 4($sp)
+        sw %r0, 0($sp)
 .end_macro
 
 .macro push(%r0, %r1, %r2, %r3, %r4, %r5, %r6, %r7, %r8)
-   addiu $sp, $sp, -36
-   sw %r8, 32($sp)
-   sw %r7, 28($sp)
-   sw %r6, 24($sp)
-   sw %r5, 20($sp)
-   sw %r4, 16($sp)
-   sw %r3, 12($sp)
-   sw %r2, 8($sp)
-   sw %r1, 4($sp)
-   sw %r0, 0($sp)
+        nop                     # Pushing 9 registers
+        addiu $sp, $sp, -36     # 9 * 4 bytes = 36
+        sw %r8, 32($sp)
+        sw %r7, 28($sp)
+        sw %r6, 24($sp)
+        sw %r5, 20($sp)
+        sw %r4, 16($sp)
+        sw %r3, 12($sp)
+        sw %r2, 8($sp)
+        sw %r1, 4($sp)
+        sw %r0, 0($sp)
 .end_macro
 
 .macro push(%r0, %r1, %r2, %r3, %r4, %r5, %r6, %r7, %r8, %r9)
-   addiu $sp, $sp, -40
-   sw %r9, 36($sp)
-   sw %r8, 32($sp)
-   sw %r7, 28($sp)
-   sw %r6, 24($sp)
-   sw %r5, 20($sp)
-   sw %r4, 16($sp)
-   sw %r3, 12($sp)
-   sw %r2, 8($sp)
-   sw %r1, 4($sp)
-   sw %r0, 0($sp)
+        nop                     # Pushing 10 registers
+        addiu $sp, $sp, -40     # 10 * 4 bytes = 40
+        sw %r9, 36($sp)
+        sw %r8, 32($sp)
+        sw %r7, 28($sp)
+        sw %r6, 24($sp)
+        sw %r5, 20($sp)
+        sw %r4, 16($sp)
+        sw %r3, 12($sp)
+        sw %r2, 8($sp)
+        sw %r1, 4($sp)
+        sw %r0, 0($sp)
 .end_macro
 
 
@@ -144,117 +153,130 @@
 # Pop Macros
 
 .macro pop(%r0)
-   lw %r0, 0($sp)
-   addiu $sp, $sp, 4
+        nop                     # Popping 1 register
+        lw %r0, 0($sp)
+        addiu $sp, $sp, 4      # 1 * 4 bytes = 4
 .end_macro
 
 .macro pop(%r0, %r1)
-   lw %r1, 4($sp)
-   lw %r0, 0($sp)
-   addiu $sp, $sp, 8
+        nop                     # Popping 2 registers
+        lw %r1, 4($sp)
+        lw %r0, 0($sp)
+        addiu $sp, $sp, 8       # 2 * 4 bytes = 8
 .end_macro
 
 .macro pop(%r0, %r1, %r2)
-   lw %r3, 12($sp)
-   lw %r2, 8($sp)
-   lw %r1, 4($sp)
-   lw %r0, 0($sp)
-   addiu $sp, $sp, 12
+        nop                     # Popping 3 registers
+        lw %r2, 8($sp)
+        lw %r1, 4($sp)
+        lw %r0, 0($sp)
+        addiu $sp, $sp, 12      # 3 * 4 bytes = 12
 .end_macro
 
 .macro pop(%r0, %r1, %r2, %r3)
-   lw %r3, 12($sp)
-   lw %r2, 8($sp)
-   lw %r1, 4($sp)
-   lw %r0, 0($sp)
-   addiu $sp, $sp, 16
+        nop                     # Popping 4 registers
+        lw %r3, 12($sp)
+        lw %r2, 8($sp)
+        lw %r1, 4($sp)
+        lw %r0, 0($sp)
+        addiu $sp, $sp, 16      # 4 * 4 bytes = 16
 .end_macro
 
 .macro pop(%r0, %r1, %r2, %r3, %r4)
-   lw %r4, 16($sp)
-   lw %r3, 12($sp)
-   lw %r2, 8($sp)
-   lw %r1, 4($sp)
-   lw %r0, 0($sp)
-   addiu $sp, $sp, 20
+        nop                     # Popping 5 registers
+        lw %r4, 16($sp)
+        lw %r3, 12($sp)
+        lw %r2, 8($sp)
+        lw %r1, 4($sp)
+        lw %r0, 0($sp)
+        addiu $sp, $sp, 20      # 5 * 4 bytes = 20
 .end_macro
 
 .macro pop(%r0, %r1, %r2, %r3, %r4, %r5)
-   lw %r5, 20($sp)
-   lw %r4, 16($sp)
-   lw %r3, 12($sp)
-   lw %r2, 8($sp)
-   lw %r1, 4($sp)
-   lw %r0, 0($sp)
-   addiu $sp, $sp, 24
+        nop                     # Popping 6 registers
+        lw %r5, 20($sp)
+        lw %r4, 16($sp)
+        lw %r3, 12($sp)
+        lw %r2, 8($sp)
+        lw %r1, 4($sp)
+        lw %r0, 0($sp)
+        addiu $sp, $sp, 24      # 6 * 4 bytes = 24
 .end_macro
 
 .macro pop(%r0, %r1, %r2, %r3, %r4, %r5, %r6)
-   lw %r6, 24($sp)
-   lw %r5, 20($sp)
-   lw %r4, 16($sp)
-   lw %r3, 12($sp)
-   lw %r2, 8($sp)
-   lw %r1, 4($sp)
-   lw %r0, 0($sp)
-   addiu $sp, $sp, 28
+        nop                     # Popping 7 registers
+        lw %r6, 24($sp)
+        lw %r5, 20($sp)
+        lw %r4, 16($sp)
+        lw %r3, 12($sp)
+        lw %r2, 8($sp)
+        lw %r1, 4($sp)
+        lw %r0, 0($sp)
+        addiu $sp, $sp, 28      # 7 * 4 bytes = 28
 .end_macro
 
 .macro pop(%r0, %r1, %r2, %r3, %r4, %r5, %r6, %r7)
-   lw %r7, 28($sp)
-   lw %r6, 24($sp)
-   lw %r5, 20($sp)
-   lw %r4, 16($sp)
-   lw %r3, 12($sp)
-   lw %r2, 8($sp)
-   lw %r1, 4($sp)
-   lw %r0, 0($sp)
-   addiu $sp, $sp, 32
+        nop                     # Popping 8 registers
+        lw %r7, 28($sp)
+        lw %r6, 24($sp)
+        lw %r5, 20($sp)
+        lw %r4, 16($sp)
+        lw %r3, 12($sp)
+        lw %r2, 8($sp)
+        lw %r1, 4($sp)
+        lw %r0, 0($sp)
+        addiu $sp, $sp, 32      # 8 * 4 bytes = 32
 .end_macro
 
 .macro pop(%r0, %r1, %r2, %r3, %r4, %r5, %r6, %r7, %r8)
-   lw %r8, 32($sp)
-   lw %r7, 28($sp)
-   lw %r6, 24($sp)
-   lw %r5, 20($sp)
-   lw %r4, 16($sp)
-   lw %r3, 12($sp)
-   lw %r2, 8($sp)
-   lw %r1, 4($sp)
-   lw %r0, 0($sp)
-   addiu $sp, $sp, 36
+        nop                     # Popping 9 registers
+        lw %r8, 32($sp)
+        lw %r7, 28($sp)
+        lw %r6, 24($sp)
+        lw %r5, 20($sp)
+        lw %r4, 16($sp)
+        lw %r3, 12($sp)
+        lw %r2, 8($sp)
+        lw %r1, 4($sp)
+        lw %r0, 0($sp)
+        addiu $sp, $sp, 36      # 9 * 4 bytes = 36
 .end_macro
 
 .macro pop(%r0, %r1, %r2, %r3, %r4, %r5, %r6, %r7, %r8, %r9)
-   lw %r9, 36($sp)
-   lw %r8, 32($sp)
-   lw %r7, 28($sp)
-   lw %r6, 24($sp)
-   lw %r5, 20($sp)
-   lw %r4, 16($sp)
-   lw %r3, 12($sp)
-   lw %r2, 8($sp)
-   lw %r1, 4($sp)
-   lw %r0, 0($sp)
-   addiu $sp, $sp, 40
+        nop                     # Popping 19 registers
+        lw %r9, 36($sp)
+        lw %r8, 32($sp)
+        lw %r7, 28($sp)
+        lw %r6, 24($sp)
+        lw %r5, 20($sp)
+        lw %r4, 16($sp)
+        lw %r3, 12($sp)
+        lw %r2, 8($sp)
+        lw %r1, 4($sp)
+        lw %r0, 0($sp)
+        addiu $sp, $sp, 40      # 10 * 4 bytes = 40
 .end_macro
 
 
 ######################
 # Aggregate Macros
 
+.macro push_t_registers()
+        nop             # Push all of the T registers
+        push $t0, $t1, $t2, $t3, $t4, $t5, $t6, $t7, $t8, $t9
+.end_macro
+
+.macro pop_t_registers()
+        nop             # Pop all of the T registers
+        pop $t0, $t1, $t2, $t3, $t4, $t5, $t6, $t7, $t8, $t9
+.end_macro
+
 .macro push_s_registers()
-   push $t0, $t1, $t2, $t3, $t4, $t5, $t6, $t7, $t8, $t9
+        nop             # Push all of the S registers
+        pop $s0, $s1, $s2, $s3, $s4, $s5, $s6, $s7
 .end_macro
 
 .macro pop_s_registers()
-   pop $t0, $t1, $t2, $t3, $t4, $t5, $t6, $t7, $t8, $t9
-.end_macro
-
-.macro push_s_registers()
-   pop $s0, $s1, $s2, $s3, $s4, $s5, $s6, $s7
-.end_macro
-
-.macro pop_s_registers()
-   pop $s0, $s1, $s2, $s3, $s4, $s5, $s6, $s7
+        nop             # Pop all of the S registers
+        pop $s0, $s1, $s2, $s3, $s4, $s5, $s6, $s7
 .end_macro
